c75f59fa3adb5ecccc9f4fcacc8dd7daf23e42de
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-04-07 17:08:11 -0700

    Create POJO entity objects

---
+++ app/src/main/java/com/example/gymlogapplication/Database/entities/GymLog.java
@@ -0,0 +1,79 @@
+package com.example.gymlogapplication.Database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+@Entity(tableName = "gymLog")
+public class GymLog {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String exercise;
+    private double weight;
+    private int reps;
+    private LocalDate localDate;
+
+    public GymLog(String exercise, double weight, int reps) {
+        this.exercise = exercise;
+        this.weight = weight;
+        this.reps = reps;
+        localDate = LocalDate.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GymLog gymLog = (GymLog) o;
+        return id == gymLog.id && Double.compare(weight, gymLog.weight) == 0 && reps == gymLog.reps && Objects.equals(exercise, gymLog.exercise) && Objects.equals(localDate, gymLog.localDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, exercise, weight, reps, localDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+}
