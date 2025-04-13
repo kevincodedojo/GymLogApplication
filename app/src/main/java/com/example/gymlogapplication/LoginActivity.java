1461259190cf3abcf49d1c060b93249f91a660cc
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-04-12 22:48:28 -0700

    added login

---
+++ app/src/main/java/com/example/gymlogapplication/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.gymlogapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gymlogapplication.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(),0);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    static Intent loginIntentFactory(Context context){
+        return new Intent(context, LoginActivity.class);
+    }
+
+
+}
--- app/src/main/java/com/example/gymlogapplication/MainActivity.java
+++ app/src/main/java/com/example/gymlogapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gymlogapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -22,6 +24,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.declink.gymlogapplication.MAIN_ACTIVITY_USER_IO";
     private ActivityMainBinding binding;
 
     private GymLogRepository repository;
@@ -43,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        loginUser();
+
+        if(loggedInUserId == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
         repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -57,6 +67,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loginUser() {
+        //Todo, create login method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
     }
 
     private void insertGymlogRecord(){
