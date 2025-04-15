6d53fd5d0aa8ad22a016001efd06b9330762b03b
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-04-14 17:12:13 -0700

    update LoginActivity

--- app/src/main/java/com/example/gymlogapplication/database/GymLogRepository.java
+++ app/src/main/java/com/example/gymlogapplication/database/GymLogRepository.java
@@ -82,7 +82,19 @@ public class GymLogRepository {
     }
 
 
-
-
-
+    public User getUserByUserName(String username) {
+        Future<User> future = GymLogDatabase.databaseWriteExecutor.submit(
+              new Callable<User>(){
+                  @Override
+                  public User call() throws Exception {
+                      return userDAO.getUserByUserName(username);
+                  }
+              });
+        try {
+            return future.get();
+        }catch (InterruptedException | ExecutionException e){
+            Log.i(MainActivity.TAG, "Problem when getting all user in the Username");
+        }
+        return null;
+    }
 }
